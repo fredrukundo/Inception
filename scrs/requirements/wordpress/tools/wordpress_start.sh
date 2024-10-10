42a16be9a325bf61c4d8b702f247158ee8b487b9
Fred Rukundo <frukundo@e3r3p12.1337.ma>
2024-10-10 19:13:29 +0100

    bullseye

--- scrs/requirements/wordpress/tools/wordpress_start.sh
+++ scrs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf";
 	chown -R www-data:www-data /var/www/*;
 	chown -R 755 /var/www/*;
 	mkdir -p /run/php/;
@@ -22,6 +22,16 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	echo "Wordpress: creating users..."
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
+
+	# Redis setup
+    echo "Wordpress: configuring Redis..."
+    wp config set WP_REDIS_HOST redis --allow-root
+    wp config set WP_REDIS_PORT 6379 --raw --allow-root
+    wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+    wp config set WP_REDIS_CLIENT phpredis --allow-root
+    wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
+    wp redis enable --allow-root
 	echo "Wordpress: set up!"
 fi
 
