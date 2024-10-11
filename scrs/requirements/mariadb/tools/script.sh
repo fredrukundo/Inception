b099c6a38f1997d125e9e67733732773ea1ab0d0
Fred Rukundo <dukefred9@gmail.com>
2024-10-11 13:17:38 +0100

    line 13 give sql passwd

--- scrs/requirements/mariadb/tools/script.sh
+++ scrs/requirements/mariadb/tools/script.sh
@@ -10,7 +10,7 @@ mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # Grant privileges to the user for the created database
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%'IDENTIFIED BY '${SQL_PASSWORD}';"
 
 # Set the root user password
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
