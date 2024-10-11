e3474432b7c2df2e300ac7d8b6567e71456eca9a
Fred Rukundo <dukefred9@gmail.com>
2024-10-11 17:07:40 +0100

    added sleep function

--- scrs/requirements/wordpress/tools/wordpress_start.sh
+++ scrs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sleep 10
 	sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf";
 	chown -R www-data:www-data /var/www/*;
 	chown -R 755 /var/www/*;
